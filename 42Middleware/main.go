0d5702eaabbb072590c0685231b78218fcb1cbf0
Divyansh-ji <Divyanshtiwary01@gmail.com>
2025-08-15 00:36:44 +0530

    logmiddleware

--- 42Middleware/main.go
+++ 42Middleware/main.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+//Task 1:
+//Create a middleware that:
+//Prints "Request received" before the handler runs.
+//Prints "Response sent" after the handler runs.
+//Attach it globally with r.Use()
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Inside handler :Home")
@@ -14,8 +21,10 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler).Methods("GET")
-	r.Use(middleware)
+	//r.HandleFunc("/", homeHandler).Methods("GET")
+	r.HandleFunc("/home", homeHandler).Methods("GET")
+	r.Use(Logmiddleware)
+
 	log.Println("Server running on:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -26,5 +35,20 @@ func middleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 		fmt.Println("Response sent")
 		next.ServeHTTP(w, r)
+	})
+}
+
+//understanding logging and timming
+
+func Logmiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log.Println(r.Method, r.RequestURI)
+		p := time.Now()
+		next.ServeHTTP(w, r)
+		e := time.Since(p)
+
+		log.Printf("the time take is ", e)
+
 	})
 }
